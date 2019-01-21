c70d0ded8c4ad44754e2a9239a1b8c2cf260b434
hayata-yamamoto <hayata.pri.yamamoto@gmail.com>
2019-01-21 13:28:26 +0900

    modified

--- redash_client/Redash.py
+++ redash_client/Redash.py
@@ -2,35 +2,34 @@ from urllib import request
 
 
 class Redash:
-    class Query:
-        def __init__(self, api_url: str):
-            """
+    def __init__(self, api_url: str):
+        """
 
-            Args:
-                api_url (str) : api endpoint
-            """
-            self.url = request.urljoin(api_url, 'queries/')
+        Args:
+            api_url (str) : api endpoint
+        """
+        self.url = request.urljoin(api_url, 'queries/')
 
-        def download_query_results(
-                self,
-                query_id: str,
-                api_key: str,
-                filename: str = 'results.csv') -> None:
-            """
+    def download_query_results(
+            self,
+            query_id: str,
+            api_key: str,
+            filename: str = 'results.csv') -> None:
+        """
 
-            Args:
-                query_id (str) : specific query id
-                api_key (str) : api authorization key
-                filename (str) : output file name
+        Args:
+            query_id (str) : specific query id
+            api_key (str) : api authorization key
+            filename (str) : output file name
 
-            Returns:
+        Returns:
 
-            """
+        """
 
-            opener = request.build_opener()
-            opener.addheaders = [('Authorization', f'Key {api_key}')]
-            request.install_opener(opener=opener)
+        opener = request.build_opener()
+        opener.addheaders = [('Authorization', f'Key {api_key}')]
+        request.install_opener(opener=opener)
 
-            url = request.urljoin(self.url, f'{query_id}/')
-            url = request.urljoin(url, 'results.csv')
-            request.urlretrieve(url=url, filename=filename)
+        url = request.urljoin(self.url, f'{query_id}/')
+        url = request.urljoin(url, 'results.csv')
+        request.urlretrieve(url=url, filename=filename)
